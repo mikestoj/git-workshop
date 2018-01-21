fe0f3584881e47a42cb882415042c24f7e563e2e
mikestoj <mikestoj19@gmail.com>
2018-01-21 15:03:49 -0500

    Rewrite fizzbuzz - BOOM BANG

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,16 +1,9 @@
 def fizzbuzz():
+
+    s = ""
     for i in range(
-        if i % 105 == 0:
-            print "fizzbuzzbizz"
-        if i % 15 == 0:
-            print "fizzbuzz"
-        elif i % 1 == 0:
-            print "fizz"
-        elif i % 5 == 0:
-            print "buzz"
-        elif i % 21 == 0:
-            print "fizzbuzz"
-        elif i % 35 == 0:
-            print "buzzbizz"
+        if i % 17 == 0:
+            s += "bang"
         else:
             print i
+
