6899f2e77bd6ffe031ab4ab3843ff827a710d836
mikestoj <mikestoj19@gmail.com>
2018-01-21 14:43:40 -0500

    fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,10 +1,16 @@
 def fizzbuzz():
     for i in range(
+        if i % 105 == 0:
+            print "fizzbuzzbizz"
         if i % 15 == 0:
             print "fizzbuzz"
         elif i % 1 == 0:
             print "fizz"
         elif i % 5 == 0:
             print "buzz"
+        elif i % 21 == 0:
+            print "fizzbuzz"
+        elif i % 35 == 0:
+            print "buzzbizz"
         else:
             print i
